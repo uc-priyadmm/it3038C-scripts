ef71480881ffe90f94d9f47762fe90f419488212
Mayank Mani Priyadarshi <89550213+uc-priyadmm@users.noreply.github.com>
2021-09-10 01:42:30 +0530

    Create sysinfo.ps1

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,10 @@
+[System.Net.Dns]::GetHostAddresses($(hostname)) | Select-String "192.*" # I am using a Macbook with powershell on it, cmdlet is only available on Windows as it uses some Windows OS APIs that aren't available on other platforms. Nearest appromixation to the command for unix would be [System.Net.Dns]::GetHostAddresses($(hostname))
+
+function IPv4{
+[System.Net.Dns]::GetHostAddresses($(hostname)) | Select-String "192.*"
+}
+write-host(IPv4) 
+$IP = IPv4   
+Write-Host(“This machine’s IP is $IP”)
+Write-Host(“This machine’s IP is {0}” -f $IP) 
+
